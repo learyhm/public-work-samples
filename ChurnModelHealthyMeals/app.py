4ddc31de0262dbc174410243253b807d43db4a11
learyhm <hannah1leary@gmail.com>
2026-07-19 15:39:03 -0500

    Update app.py

--- ChurnModelHealthyMeals/app.py
+++ ChurnModelHealthyMeals/app.py
@@ -8,11 +8,12 @@ import pickle
 # ============================================================
 @st.cache_resource
 def load_artifacts():
-    with open("ChurnModelHealthyMeals/churn_encoder_healthy_meals.pkl", "rb") as f:
+    with open("ChurnModelHealthyMeals/churn_xgb_healthy_meals.pkl", "rb") as f:
         model = pickle.load(f)
     with open("ChurnModelHealthyMeals/churn_encoder_healthy_meals.pkl", "rb") as f:
         encoder = pickle.load(f)
     return model, encoder
+
 
 model, encoder = load_artifacts()
 
